affba6a44ac209bd4fa5ae29cc9f89c96b1cf390
unknown <bubbastinx@gmail.com>
2018-07-10 20:12:15 -0600

    installing scripts

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./Routes');
+const models = require('./Models');
+
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/myapp';
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.use(routes);
+
+
+mongoose.connect(MONGODB_URI)
+mongoose.Promise = global.Promise;
+let db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+// mongoose.connect( MONGODB_URI);
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on http://localhost:${PORT}`);
+});
